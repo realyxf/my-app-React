a336ed47047c1ce60ce55dfb41d95d6b6ed91f95
realyxf <997915421@qq.com>
2020-08-03 22:52:18 +0800

    [Counter] [Fennie] feature: use CounterGroup means the number ofCounter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
-import * as Action from "./Action";
+import * as Action from "./Component/Counter/Action";
 import { connect } from "react-redux";
-import store from "./Store";
+import CounterGroup from './Component/CounterGroup'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <CounterGroup/>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-class App extends Component {
-
-  render() {
-    
-    const { increment, decrement } = this.props;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-        <button onClick={() => decrement()}>-</button>
-        <label>{this.props.count}</label>
-        <button onClick={() => increment()}>+</button>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CounterGroup/>
+      </header>
+    </div>
+  );
 }
 
-const mapStateToProps = state => ({
-  count: state
-});
+export default App;
 
-export default connect(mapStateToProps,Action)(App);
+
+// class App extends Component {
+
+//   render() {
+    
+//     const { increment, decrement } = this.props;
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//         <button onClick={() => decrement()}>-</button>
+//         <label>{this.props.count}</label>
+//         <button onClick={() => increment()}>+</button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => ({
+//   count: state
+// });
+
+// export default connect(mapStateToProps,Action)(App);
 
 //onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
--- src/Component/Counter/index.jsx
+++ src/Component/Counter/index.jsx
@@ -1,48 +1,60 @@
-import React from 'react'
+import React, { Component } from "react";
+import * as Action from "./Action";
+import { connect } from "react-redux";
 
 class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0,
-            size: 0
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: 0,
+    //         size: 0
+    //     }
+    // }
 
-    onIcrease = () => {
-        this.setState((prevState) => ({
-            value: prevState.value + 1
-        }));
-        this.props.onIcrease();
-    }
+    // onIcrease = () => {
+    //     this.setState((prevState) => ({
+    //         value: prevState.value + 1
+    //     }));
+    //     this.props.onIcrease();
+    // }
 
-    onReduce = () => {
-        this.setState((prevState) => ({
-            value: prevState.value - 1
-        }));
-        this.props.onReduce();
-    }
+    // onReduce = () => {
+    //     this.setState((prevState) => ({
+    //         value: prevState.value - 1
+    //     }));
+    //     this.props.onReduce();
+    // }
 
-    static getDerivedStateFromProps(props, state) {
+    // static getDerivedStateFromProps(props, state) {
 
-        if (props.size !== state.size) {
-            return {
-                size: props.size,
-                value: 0
-            };
-        }
-        return null;
-    }
+    //     if (props.size !== state.size) {
+    //         return {
+    //             size: props.size,
+    //             value: 0
+    //         };
+    //     }
+    //     return null;
+    // }
 
     render() {
+
+        const { increment, decrement } = this.props;
+
+
         return (
             <div>
-                <button onClick={this.onReduce}>-</button>
-                <label>{this.state.value}</label>
-                <button onClick={this.onIcrease}>+</button>
+                <button onClick={() => increment()}>-</button>
+                <label>{this.props.count}</label>
+                <button onClick={() => increment()}>+</button>
             </div>
         )
     }
 }
 
-export default Counter;
+const mapStateToProps = state => ({
+    count: state
+  });
+
+//export default Counter;
+
+export default connect(mapStateToProps,Action)(Counter);
